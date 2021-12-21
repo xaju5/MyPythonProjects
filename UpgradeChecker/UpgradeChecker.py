1185912b01ca38d7e6a34292a6b819f9b41ced0b
Xabier <xajubdev@gmail.com>
2021-12-21 21:13:30 +0100

    string algorithm

--- UpgradeChecker/UpgradeChecker.py
+++ UpgradeChecker/UpgradeChecker.py
@@ -10,8 +10,8 @@ import pandas as pd
 filename_Original = "NuevosCambiosRaw.txt"
 filename_Filtered = "CambiosAAA.txt"
 teamName = "AAA"
-#TODO: Change to dataframe
-upgradeData = pd.Series(index=["Tech","Network","Location","Version"])
+upgradeDF = pd.DataFrame(columns=["Network","New Version"])
+
 
 
 #Step 1: Read data
@@ -32,5 +32,35 @@ file.close()
 
 #Step3: Data processing
 for line in teamLines: #TODO
-    print(line.split("-"))
-    print('\n')
+    if(line.find("pgrade") >= 0):
+        splitedLine = line.split(" - ")
+        techName = splitedLine[1].split(" ")[0]
+        networkName = splitedLine[1].split(" ")[1]
+       
+        if(len(splitedLine[2]) == 1):
+            location = splitedLine[2]
+        elif(splitedLine[2].split(" ")[0] == "Ramal"):
+            location = splitedLine[2].split(" ")[0] + " " + splitedLine[2].split(" ")[1]
+        else:
+            print(splitedLine[2])
+            location = splitedLine[2].split(" ")[1]        
+         
+        upgradeTextPos = line.find("pgrade a")    
+        if(upgradeTextPos >= 0):
+            upgradeText = line.split("pgrade a ")
+            versionName = upgradeText[1].split(" ")[0]
+            versionName = versionName[:-1]
+        else:
+            upgradeTextPos = line.find("pgrade v")    
+            if(upgradeTextPos >= 0):
+                upgradeText = line.split("pgrade v")
+                versionName = upgradeText[1].split(" ")[0]
+       
+        upgradeDF.loc[techName + " " + location] =[networkName,versionName]
+
+#Step 4: Representation
+print("Number of total changes: " + str(len(rawLines)))        
+print("Number of team changes: " + str(len(teamLines)))
+upgradeDF = upgradeDF.sort_index()
+print("Results: ")
+print(upgradeDF)
