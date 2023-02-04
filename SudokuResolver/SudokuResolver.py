8d965b9a1fb483327684fc6482de10954134054f
Xabier <xajubdev@gmail.com>
2023-02-05 00:38:08 +0100

    Update SudokuResolver.py
    Main menu options, plot, save and load functions added

--- SudokuResolver/SudokuResolver.py
+++ SudokuResolver/SudokuResolver.py
@@ -7,16 +7,47 @@ Created on Sat Feb  4 16:30:05 2023
 
 import numpy as np
 
-#Parameters
-solution = np.zeros((9,9))
-unsolved = np.zeros((9,9))
-option = None
+#==========
+#PARAMETERS
+#==========
 
-#FUNCTIONS
+solution = np.zeros((9,9))
+sudoku = np.zeros((9,9))
+option = None
+filename='arrays.npz'
+
+#==============
+#MENU FUNCTIONS
+#==============
+def printMenu():
+    print("\nWhat do you want to do?"+
+          "\n0-Exit"+
+          "\n1-Insert sudoku"+
+          "\n2-Insert solution"+
+          "\n3-Print sudoku"+
+          "\n4-Print solution"+
+          "\n5-Solve sudoku"+
+          "\n"+
+          "\n6-Load arrays"+
+          "\n7-Save arrays")
+    
 def fillArray(array):
+    """
+    Fills the array with the input of the user
+
+    Parameters
+    ----------
+    array : TYPE
+        The array that will be filled.
+
+    Returns
+    -------
+    None.
+
+    """
     
     for r in range(9):
-        print("Insert "+str(r)+"row: ")
+        print("\nInsert "+str(r)+"row (write 0 for unknow values): ")
         row = input()
         c = 0
         for value in row:
@@ -24,25 +55,88 @@ def fillArray(array):
             c +=1
             
 def printArray(array):
-    print(array)
+    """
+    Print the array divided by lines, for easier reading
 
+    Parameters
+    ----------
+    array : TYPE
+        array to print.
 
-#MAIN
+    Returns
+    -------
+    None.
+
+    """
+    print("\n")
+    numrow = 0
+    for row in array:
+        #Checks if is needed to print a horizontal line
+        if numrow % 3 == 0 and numrow != 0:
+            print("--- --- ---")
+        numrow += 1
+        
+        numcolum = 0
+        for colum in row:
+            #checks if it is needed to print a vertical line
+            if numcolum % 3 == 0 and numcolum != 0:
+                print("|",end='')
+            numcolum += 1
+            
+            #actual array[r,c] is printed:
+            print(int(colum),end='')
+        print("\n")
+            
+def loadArray():
+    ans = input("\nAre you sure that you want to LOAD the file?(y/n):")
+    if ans == 'y':
+        with np.load(filename) as file:
+            global sudoku, solution
+            sudoku = file['a']
+            solution = file['b']
+        print("\n>>Arrays loaded")
+    else:
+        print("\nOperation aborted")
+
+def saveArray():
+    ans = input("\nAre you sure that you want to OVERWRITE the file?(y/n):")
+    if ans == 'y':
+        np.savez(filename,a=sudoku,b=solution)
+        print("\n>>Arrays saved")
+    else:
+        print("\nOperation aborted")
+ 
+#===================
+#ALGORITHM FUNCTIONS
+#===================
+def resolveSudoku():
+    print("TODO")
+ 
+#=============
+#MAIN fUNCTION
+#=============
 while option != "0":
-    print("What do you want to do? \n0-Exit \n1-insert unsolved array \n2-Insert solution \n3-print unsolved \n4-print solution \n5-save arrays \nChoose one:")
-    option = input()
+    printMenu()
+    option = input("Choose one:")
     if option == "0":
-        print("exit")
-        break
-    if option == "1":
-        fillArray(unsolved)
-    if option == "2":
+        print("\n>>EXIT")
+    elif option == "1":
+        fillArray(sudoku)
+    elif option == "2":
         fillArray(solution)
-    if option == "3":
-        printArray(unsolved)
-    if option == "4":
+    elif option == "3":
+        printArray(sudoku)
+    elif option == "4":
         printArray(solution)
-    if option == "5":
-        print("TODO")
+    elif option == "5":
+        resolveSudoku()
+    elif option == "6":
+        loadArray()
+        
+    elif option == "7":
+        saveArray()
+    else:
+        print("\n>>Wrong value!!")
+          
    
     
